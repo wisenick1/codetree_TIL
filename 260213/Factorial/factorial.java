5d1e7df8ec8240dac98fe2ab5d5e04aa2c958483
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-13 08:19:34 +0900

    Title: Factorial, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260213/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(fac(n));
+    }
+
+    private static int fac(int n) {
+        if(n == 0) return 1;
+
+        return fac(n - 1) * n;
+    }
+}

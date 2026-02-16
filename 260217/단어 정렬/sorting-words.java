65d0325394fc0c7b64c7b1ad3f39bda8e6b3a653
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-17 01:11:25 +0900

    Title: 단어 정렬, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260217/단어 정렬/sorting-words.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] wordList = new String[n];
+        for (int i = 0; i < n; i++) {
+            wordList[i] = sc.next();
+        }
+        // Please write your code here.
+        Arrays.sort(wordList);
+        for (int i = 0; i < n; i++) {
+            System.out.println(wordList[i]);
+        }
+    }
+}

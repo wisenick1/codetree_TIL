fd5d9222d6c495d50c40e61ab589e8585479cdf0
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-17 01:08:00 +0900

    Title: 문자열 정렬, Time: 120ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260217/문자열 정렬/string-sort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        // Please write your code here.
+        char[] arr = s.toCharArray();
+        Arrays.sort(arr);
+        String str = new String(arr);
+        System.out.println(str);
+    }
+}

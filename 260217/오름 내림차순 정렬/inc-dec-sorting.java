a85b4680ffba719a51c77da9bc14d6b34eb47115
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-17 01:04:18 +0900

    Title: 오름 내림차순 정렬, Time: 303ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260217/오름 내림차순 정렬/inc-dec-sorting.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        Integer[] arr2 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(arr2, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+    }
+}

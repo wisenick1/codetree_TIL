ac5b44f108d005a961b6f0d85952ce39516a77a5
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-19 15:41:20 +0900

    Title: 키를 기준으로 정렬, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260219/키를 기준으로 정렬/sort-by-height.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        Person[] arr = new Person[n];
+        for(int i = 0; i < n; i++) {
+            String name = sc.next();
+            int height = sc.nextInt();
+            int weight = sc.nextInt();
+            arr[i] = new Person(name, height, weight);
+        }
+
+        Arrays.sort(arr);
+
+        for(int i = 0; i < n; i++) {
+            System.out.println(arr[i].name + " " + arr[i].height + " " + arr[i].weight);
+        }
+    }
+
+
+}
+
+class Person implements Comparable<Person> {
+    String name;
+    int height;
+    int weight;
+
+    public Person(String name, int height, int weight) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Person p) {
+        return this.height - p.height;
+    }
+}

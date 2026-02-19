4f313d3105062f01275bd1744ee076f8383601f2
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-19 15:14:58 +0900

    Title: 코드네임, Time: 132ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260219/코드네임/code-name.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Please write your code here.
+        Agent[] arr = new Agent[5];
+        for(int i = 0; i < 5; i++) {
+            String codeName = sc.next();
+            int score = sc.nextInt();
+            arr[i] = new Agent(codeName, score);
+        }
+
+        int idx = 0;
+        int min = Integer.MIN_VALUE;
+        for(int i = 0; i < 5; i++) {
+            if(arr[i].score < min) {
+                idx = i;
+                min = arr[i].score;
+            }
+        }
+
+        System.out.println(arr[idx].codeName + " " + arr[idx].score);
+
+
+    }
+}
+
+class Agent {
+    String codeName;
+    int score;
+
+    public Agent(String codeName, int score) {
+        this.codeName = codeName;
+        this.score = score;
+    }
+}

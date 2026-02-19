4d016f0d85b7e74370686980dcbda9aa6151cfc4
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-20 07:53:28 +0900

    Title: Date to Date, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 260220/Date to Date/date-to-date.java
+++ 260220/Date to Date/date-to-date.java
@@ -12,16 +12,16 @@ public class Main {
         int days1 = 0;
         int days2 = 0;
 
-        for(int i = 0; i < m1; i++) {
+        for(int i = 0; i < m1 - 1; i++) {
             days1 += numofDays[i];
         }
         days1 += d1;
 
-        for(int i = 0; i < m2; i++) {
+        for(int i = 0; i < m2 - 1; i++) {
             days2 += numofDays[i];
         }
         days2 += d2;
 
-        System.out.println(days2 - days1);
+        System.out.println(days2 - days1 + 1);
     }
 }

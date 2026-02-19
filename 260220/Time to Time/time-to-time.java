56446a5e2626e2d4967679e4410001cd90b0723b
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-20 07:41:10 +0900

    Title: Time to Time, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260220/Time to Time/time-to-time.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        // Please write your code here.
+        int minuteA = 60 * a + b;
+        int minuteB = 60 * c + d;
+
+        System.out.println(minuteB - minuteA);
+    }
+}

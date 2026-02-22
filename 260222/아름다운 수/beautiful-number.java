ce426a6e13ed0716a493b8084f66e5ba44f44bf9
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-02-22 10:09:58 +0900

    Title: 아름다운 수, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 260222/아름다운 수/beautiful-number.java
+++ 260222/아름다운 수/beautiful-number.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 public class Main {
-    static int len;
     static int result;
     static int[] arr;
+    static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         // Please write your code here.
-        len = n;
         dfs(0);
         System.out.println(result);
     }
 
     
     private static void dfs(int cnt) {
-        if(cnt == len) {
+        if(cnt == n) {
             result++;
             return;
         }
 
-        if(cnt > len) return;
+        if(cnt > n) return;
 
-        for(int i = 1; i < 4; i++) {
+        for(int i = 1; i <= 4; i++) {
             dfs(cnt + i);
         }
     }

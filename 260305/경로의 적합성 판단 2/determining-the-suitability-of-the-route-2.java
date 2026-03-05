a5bba0333af04383c41990a135c5730bc50ad0bf
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-03-05 22:14:27 +0900

    Title: 경로의 적합성 판단 2, Time: 237ms, Memory: 21MB, Status: Wrong Answer - Codetree

---
+++ 260305/경로의 적합성 판단 2/determining-the-suitability-of-the-route-2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m, k;
+    static int[] parent;
+    static int[] path;
+    static boolean isSame = true;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        
+        init();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < k; i++) {
+            path[i] = Integer.parseInt(st.nextToken());
+        }
+        // Please write your code here.
+
+        System.out.println(check() ? 1 : 0);
+
+    }
+
+    private static void init() {
+        parent = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        path = new int[k];
+    }
+
+    private static void union(int a, int b) {
+        int x = find(a);
+        int y = find(b);
+
+        parent[y] = x;
+    }
+
+    private static int find(int x) {
+        if(parent[x] == x) return x;
+        int root = find(parent[x]);
+        parent[x] = root;
+        return root;
+    }
+
+    private static boolean check() {
+        for (int i = 0; i < k - 1; i++) {
+            if(parent[path[i]] != parent[path[i + 1]]) return false;
+        }
+        return true;
+    }
+
+
+}

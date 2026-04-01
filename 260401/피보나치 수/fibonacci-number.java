9075a4419391e5bf2416f09e4c095676b2c46545
Jae IL Kang <51376082+wisenick1@users.noreply.github.com>
2026-04-01 12:14:43 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/피보나치 수/fibonacci-number.java
+++ 260401/피보나치 수/fibonacci-number.java
@@ -11,7 +11,7 @@ public class Main {
         dp[2] = 1;
 
         for(int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            if(dp[i] == 0) dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         System.out.println(dp[n]);
